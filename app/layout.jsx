f862c32c58ff296ac4338f766f03c454474146eb
njiutheguru <stephennjiu@gmail.com>
2025-05-03 13:19:13 +0300

    initial conf

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,18 @@
 import '@/assets/styles/globals.css';
 
+// metadata
+export const metadata = {
+    title: 'KejaSpot',
+    keywords: 'Real Estate, Property, Kenya, Keja, airbnb in kenya, Booking, Rentals, rent a house',
+    description: 'KejaSpot. The best place to find your next home in Africa. Rent for hours, days, weeks or months.',
+}
+
 const MainLayout = ( {children} ) => {
   return (
     <html>
         <body>
             <main>
-              <div>{children}</div>
+              {children}
             </main>
         </body>
     </html>

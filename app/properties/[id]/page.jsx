f27b9b47a7bec19d7615ed9fbf1917efb23ba6c3
njiutheguru <stephennjiu@gmail.com>
2025-05-08 12:31:51 +0300

    cloudinary connected-1

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -5,11 +5,21 @@ import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import PropertyDetails from "@/components/PropertyDetails";
 import Link from "next/link";
 import { FaArrowLeft } from 'react-icons/fa'
+import { convertToSerializableObject } from "@/utils/convertToObject";
 // import { useParams } from "next/navigation";
 
 const PropertyPage = async ({ params }) => {
   await connectDB();
-  const property = await Property.findById(params.id).lean()
+  const propertyDoc = await Property.findById(params.id).lean()
+  const property = convertToSerializableObject(propertyDoc)
+
+  if (!property){
+    return (
+      <h1 className="text-center text-2xl font-bold mt-10">
+        Property Not Found
+      </h1>
+    )
+  }
 
   return (
     <>
